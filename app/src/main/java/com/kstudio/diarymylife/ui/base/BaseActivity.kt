b2fbb0cec45812a9eecbf0dadbd11505c7dfa2b6
vendor353 <kriratik@odds.team>
2021-11-14 22:25:45 +0700

    [Add] transition when navigate

--- app/src/main/java/com/kstudio/diarymylife/ui/base/BaseActivity.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/base/BaseActivity.kt
@@ -1,6 +1,23 @@
 package com.kstudio.diarymylife.ui.base
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
+import com.kstudio.diarymylife.R
+import com.kstudio.diarymylife.ui.journal.JournalDetailActivity
 
-class BaseActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
+
+    fun navigateToActivity(activity: Class<*>?) {
+        val intent = Intent(this, activity)
+        startActivity(intent)
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(
+            R.anim.slide_in_left,
+            R.anim.slide_out_right
+        )
+    }
 }
--- app/src/main/java/com/kstudio/diarymylife/ui/base/BaseFragment.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/base/BaseFragment.kt
@@ -1,8 +1,17 @@
 package com.kstudio.diarymylife.ui.base
 
+import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.kstudio.diarymylife.R
+import com.kstudio.diarymylife.ui.journal.JournalDetailActivity
 
 open class BaseFragment : Fragment() {
     protected var _binding: ViewBinding? = null
+
+    fun navigateToActivity(activity: Class<*>?) {
+        val intent = Intent(context, activity)
+        startActivity(intent)
+        getActivity()?.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
 }
