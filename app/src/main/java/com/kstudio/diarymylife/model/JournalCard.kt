e8484ea9e85778a69df6ee666fec7869b3c16976
vendor353 <kriratik@odds.team>
2021-11-29 00:26:53 +0700

    [Edit] card for swipe

--- app/src/main/java/com/kstudio/diarymylife/model/JournalCard.kt
+++ app/src/main/java/com/kstudio/diarymylife/model/JournalCard.kt
@@ -1,6 +1,7 @@
 package com.kstudio.diarymylife.model
 
 import android.os.Parcelable
+import com.kstudio.diarymylife.ui.base.SwipeEvent.SwipeState
 import kotlinx.parcelize.Parcelize
 import java.util.*
 import kotlin.collections.ArrayList
@@ -13,5 +14,6 @@ data class JournalCard(
     val mood: String,
     val activity: ArrayList<String?>?,
     val timestamp: Date,
-    val imageId: String
+    val imageId: String,
+    var state : SwipeState = SwipeState.NONE
 ): Parcelable
---
+++ app/src/main/java/com/kstudio/diarymylife/ui/base/SwipeEvent/SwipeState.kt
@@ -0,0 +1,5 @@
+package com.kstudio.diarymylife.ui.base.SwipeEvent
+
+enum class SwipeState {
+    NONE, LEFT, RIGHT, LEFT_RIGHT
+}
