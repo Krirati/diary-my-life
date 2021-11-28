7ea1b3da60c58d887a44572965263916453b1b55
vendor353 <kriratik@odds.team>
2021-11-29 00:20:32 +0700

    [EDIT] color status bar

--- app/src/main/java/com/kstudio/diarymylife/ui/base/BaseFragment.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/base/BaseFragment.kt
@@ -4,14 +4,25 @@ import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import com.kstudio.diarymylife.R
-import com.kstudio.diarymylife.ui.journal.JournalDetailActivity
+import com.kstudio.diarymylife.utils.Keys
 
 open class BaseFragment : Fragment() {
     protected var _binding: ViewBinding? = null
 
-    fun navigateToActivity(activity: Class<*>?) {
+    fun navigateToActivity(activity: Class<*>?, journalId: Int?) {
         val intent = Intent(context, activity)
         startActivity(intent)
-        getActivity()?.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        intent.putExtra(Keys.JOURNAL_ID, journalId)
+        requireActivity().overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+    }
+
+    fun finish(
+        enterAnimation: Int = R.anim.slide_in_left,
+        exitAnimation: Int = R.anim.slide_out_right
+    ) {
+        requireActivity().run {
+            finish()
+            overridePendingTransition(enterAnimation, exitAnimation)
+        }
     }
 }
