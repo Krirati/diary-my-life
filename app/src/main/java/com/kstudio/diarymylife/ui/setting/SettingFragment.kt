16cb1be51db28da2ecd5f402ec86d9e9bdffa791
vendor353 <kriratik@odds.team>
2022-12-03 15:36:52 +0700

    [Edit] Refactor background color and add new icon

--- app/src/main/java/com/kstudio/diarymylife/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/setting/SettingFragment.kt
@@ -1,12 +1,41 @@
 package com.kstudio.diarymylife.ui.setting
 
+import android.os.Bundle
+import android.view.View
+import com.kstudio.diarymylife.R
 import com.kstudio.diarymylife.databinding.FragmentSettingBinding
 import com.kstudio.diarymylife.ui.base.BaseFragment
 
-class SettingFragment: BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
-    private lateinit var viewModel: SettingViewModel
+class SettingFragment : BaseFragment<FragmentSettingBinding>(FragmentSettingBinding::inflate) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindingView()
+    }
+
     override fun bindingView() {
-        TODO("Not yet implemented")
+        binding.apply {
+            settingUsername.apply {
+                setTitle("Profile")
+                setIcon(R.drawable.ic_user)
+            }
+            settingNoti.apply {
+                setTitle("Notification")
+                setIcon(R.drawable.ic_notification)
+            }
+            settingPrivacy.apply {
+                setTitle("Privacy and security")
+                setIcon(R.drawable.ic_shield)
+            }
+            settingCredit.apply {
+                setTitle("Credit")
+                setIcon(R.drawable.ic_credit_card)
+            }
+            settingClearData.apply {
+                setTitle("Clear data")
+                setIcon(R.drawable.ic_database)
+            }
+        }
     }
 
     override fun handleOnBackPress() {
