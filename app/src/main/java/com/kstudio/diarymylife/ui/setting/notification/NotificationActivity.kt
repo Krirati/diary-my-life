026104fced73e1a3827f06053a1a63371c521370
vendor353 <kriratik@odds.team>
2022-12-21 23:26:38 +0700

    [Add] NotificationActivity.kt

--- app/src/main/java/com/kstudio/diarymylife/ui/setting/notification/NotificationActivity.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/setting/notification/NotificationActivity.kt
@@ -1,11 +1,24 @@
 package com.kstudio.diarymylife.ui.setting.notification
 
 import android.os.Bundle
+import com.kstudio.diarymylife.databinding.ActivityNotificationBinding
 import com.kstudio.diarymylife.ui.base.BaseActivity
 
 class NotificationActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityNotificationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityNotificationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding()
+    }
 
+    private fun binding() = with(binding) {
+        back.setOnClickListener { handleBackPass() }
+    }
+
+    private fun handleBackPass() {
+        onBackPressedDispatcher.onBackPressed()
     }
 }
