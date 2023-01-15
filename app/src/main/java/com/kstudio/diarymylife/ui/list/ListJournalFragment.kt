49ae1927debc06b6a5f083294e83f82031ac916a
krirati <kriratik@odds.team>
2023-01-15 23:04:58 +0700

    [Edit] list fragment

--- app/src/main/java/com/kstudio/diarymylife/ui/list/ListJournalFragment.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/list/ListJournalFragment.kt
@@ -38,7 +38,7 @@ class ListJournalFragment : BaseFragment<FragmentListJournalBinding>(
     override fun bindingView() {
         binding.apply {
             cardAverage.bind("Mood avg", 5, R.drawable.pie_chart_24)
-            cardTotal.bind("Total mood", 5, R.drawable.ic_calendar_24)
+            cardTotal.bind("Total mood", 5, R.drawable.ic_bar_chart_24)
             recyclerview.apply {
                 layoutManager = GridLayoutManager(context, 1, GridLayoutManager.VERTICAL, false)
                 adapter = moodAdapter
