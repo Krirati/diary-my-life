f322a60f69407a0328352261ec430617f5894f25
krirati <kriratik@odds.team>
2023-10-02 16:26:30 +0700

    [Update] : show bar when not have data

--- app/src/main/java/com/kstudio/diarymylife/ui/summary/SummaryMoodViewModel.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/summary/SummaryMoodViewModel.kt
@@ -4,15 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.kstudio.diarymylife.R
-import com.kstudio.diarymylife.data.mood.MoodRepository
 import com.kstudio.diarymylife.domain.GetMoodsAndActivityUseCase
 import com.kstudio.diarymylife.domain.model.MoodViewType
 import com.kstudio.diarymylife.ui.base.BaseViewModel
 import com.kstudio.diarymylife.utils.BackgroundTheme
-import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Poor
-import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Excellent
 import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Average
+import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Excellent
 import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Good
+import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Poor
 import com.kstudio.diarymylife.utils.BackgroundTheme.Companion.Very_Poor
 import com.kstudio.diarymylife.widgets.custom_chart.BarData
 import kotlinx.coroutines.launch
@@ -50,23 +49,29 @@ class SummaryMoodViewModel constructor(
     private fun mappingDataToChart(moodViewTypes: List<MoodViewType>) {
         val dataList = ArrayList<BarData>()
         var maxValue = 0
-        moodViewTypes.groupBy { it.data?.mood }.forEach { (moodType, moods) ->
+        moodViewTypes.groupBy { it.data?.mood }.forEach { (_, moods) ->
             maxValue = if (moods.size > maxValue) moods.size else maxValue
-            val data = when (moodType) {
-                Very_Poor -> BarData(5, moods.size.toFloat(), R.color.mood_very_pool_drop, R.drawable.crying)
-                Poor -> BarData(4, moods.size.toFloat(), R.color.mood_pool_drop, R.drawable.sad)
-                Average -> BarData(
-                    3,
-                    moods.size.toFloat(),
-                    R.color.mood_average_drop,
-                    R.drawable.neutral
-                )
-                Good -> BarData(2, moods.size.toFloat(), R.color.mood_good_drop, R.drawable.smile)
-                Excellent-> BarData(1, moods.size.toFloat(), R.color.mood_excellent_drop, R.drawable.lol)
-                else -> null
-            }
-            data?.let { dataList.add(it) }
         }
+        val group = moodViewTypes.groupBy { it.data?.mood }
+        val veryPoor = group[Very_Poor]?.size ?: 0
+        val poor = group[Poor]?.size ?: 0
+        val average = group[Average]?.size ?: 0
+        val good = group[Good]?.size ?: 0
+        val excellent = group[Excellent]?.size ?: 0
+        val veryPoorData =
+            BarData(5, veryPoor.toFloat(), R.color.mood_very_pool_drop, R.drawable.crying)
+        val poorData = BarData(4, poor.toFloat(), R.color.mood_pool_drop, R.drawable.sad)
+        val averageData =
+            BarData(3, average.toFloat(), R.color.mood_average_drop, R.drawable.neutral)
+        val goodData = BarData(2, good.toFloat(), R.color.mood_good_drop, R.drawable.smile)
+        val excellentData =
+            BarData(1, excellent.toFloat(), R.color.mood_excellent_drop, R.drawable.lol)
+        dataList.add(veryPoorData)
+        dataList.add(poorData)
+        dataList.add(averageData)
+        dataList.add(goodData)
+        dataList.add(excellentData)
+
         dataList.sortBy { it.barOrder }
 
         _barData.postValue(maxValue to dataList)
