7786f47c6f44fc55fe1ca08858922c70148ad3df
krirati <kriratik@odds.team>
2023-11-11 23:37:56 +0700

    [Test] : test pipeline test step comment rive animation

--- app/src/main/java/com/kstudio/diarymylife/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/kstudio/diarymylife/ui/splash/SplashActivity.kt
@@ -2,9 +2,6 @@ package com.kstudio.diarymylife.ui.splash
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import app.rive.runtime.kotlin.RiveArtboardRenderer
-import app.rive.runtime.kotlin.core.PlayableInstance
-import com.kstudio.diarymylife.MainActivity
 import com.kstudio.diarymylife.databinding.ActivitySplashBinding
 import com.kstudio.diarymylife.ui.base.BaseActivity
 
@@ -19,34 +16,34 @@ class SplashActivity : BaseActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        bindingEvent()
+//        bindingEvent()
     }
 
-    private fun bindingEvent() = with(binding) {
-        rive.apply {
-            registerListener(listener)
-        }
+//    private fun bindingEvent() = with(binding) {
+//        rive.apply {
+//            registerListener(listener)
+//        }
+//
+//    }
 
-    }
-
-    val listener = object : RiveArtboardRenderer.Listener {
-        override fun notifyPlay(animation: PlayableInstance) {/* Do nothing*/
-        }
-
-        override fun notifyStateChanged(
-            stateMachineName: String,
-            stateName: String
-        ) {/* Do nothing*/
-        }
-
-        override fun notifyPause(animation: PlayableInstance) {/* Do nothing*/
-        }
-
-        override fun notifyStop(animation: PlayableInstance) {
-            navigateToActivity(MainActivity::class.java)
-        }
-
-        override fun notifyLoop(animation: PlayableInstance) {/* Do nothing*/
-        }
-    }
+//    val listener = object : RiveArtboardRenderer.Listener {
+//        override fun notifyPlay(animation: PlayableInstance) {/* Do nothing*/
+//        }
+//
+//        override fun notifyStateChanged(
+//            stateMachineName: String,
+//            stateName: String
+//        ) {/* Do nothing*/
+//        }
+//
+//        override fun notifyPause(animation: PlayableInstance) {/* Do nothing*/
+//        }
+//
+//        override fun notifyStop(animation: PlayableInstance) {
+//            navigateToActivity(MainActivity::class.java)
+//        }
+//
+//        override fun notifyLoop(animation: PlayableInstance) {/* Do nothing*/
+//        }
+//    }
 }
